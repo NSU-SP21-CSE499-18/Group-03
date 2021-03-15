f96aad29469d7112f76fd54bfc1163b7e1dc39c4
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-03-15 22:02:55 +0600

    test auto path filter in ci/cd

--- Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/MainActivity.kt
+++ Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/MainActivity.kt
@@ -2,10 +2,12 @@ package study.cse499.socialpostscheduler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("tag", "test auto filter");
     }
 }
