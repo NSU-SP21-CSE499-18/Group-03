4d93a08a8e7d3ffa3a831e2b302122f4a90e031c
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-05-16 14:25:06 +0600

    added fragment factory for ui testing

---
+++ Project Code/SocialPostScheduler/app/src/androidTest/java/study/cse499/socialpostscheduler/ui/LoginFragmentTest.kt
@@ -0,0 +1,13 @@
+package study.cse499.socialpostscheduler.ui
+
+import androidx.test.filters.MediumTest
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.*
+
+@MediumTest
+@HiltAndroidTest
+@ExperimentalCoroutinesApi
+class LoginFragmentTest{
+
+}
