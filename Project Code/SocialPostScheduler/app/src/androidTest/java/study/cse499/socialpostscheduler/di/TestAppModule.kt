8dfeec3ca1e3f081bedf4b1b0ba0274fbc82b9a8
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-05-16 15:56:47 +0600

    missing module annotiation added, added jcenter

--- Project Code/SocialPostScheduler/app/src/androidTest/java/study/cse499/socialpostscheduler/di/TestAppModule.kt
+++ Project Code/SocialPostScheduler/app/src/androidTest/java/study/cse499/socialpostscheduler/di/TestAppModule.kt
@@ -2,11 +2,16 @@ package study.cse499.socialpostscheduler.di
 
 import android.content.Context
 import androidx.room.Room
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import study.cse499.socialpostscheduler.data.local.ScheduleDatabase
 import javax.inject.Named
 
+@Module
+@InstallIn(SingletonComponent::class)
 class TestAppModule {
     @Provides
     @Named("test_db")
