6c61bdb27240f22a7ba833d98be00107f6396b89
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-06-26 17:08:39 +0600

    app crash fixed due to missing annotiation

--- Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/ui/MainActivity.kt
+++ Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/ui/MainActivity.kt
@@ -3,8 +3,10 @@ package study.cse499.socialpostscheduler.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
 import study.cse499.socialpostscheduler.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
