0d6924cff981dd708b86db4a10d774b59f1c00cb
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-06-27 22:51:06 +0600

    facebook login added

--- Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/ui/LoginFragment.kt
+++ Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/ui/LoginFragment.kt
@@ -4,22 +4,56 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginBehavior
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import kotlinx.android.synthetic.main.fragment_login.*
 import study.cse499.socialpostscheduler.R
 import study.cse499.socialpostscheduler.viewmodel.LoginViewModel
-import study.cse499.socialpostscheduler.viewmodel.ScheduleViewModel
+import timber.log.Timber
+
 
 class LoginFragment (): Fragment(R.layout.fragment_login) {
     lateinit var viewModel : LoginViewModel
+    lateinit var loginManager: LoginManager
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(LoginViewModel::class.java)
         loginButton.setOnClickListener {
-            findNavController().navigate(
-                LoginFragmentDirections.actionLoginFragmentToScheduleFragment()
-            )
+            initFacebookLogin()
         }
 
+    }
+
+    private fun initFacebookLogin(){
+        loginManager = LoginManager.getInstance()
+        val callbackManager = CallbackManager.Factory.create()
+        loginManager.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                Timber.d("success")
+            }
+
+            override fun onCancel() {
+                Timber.d("cancel")
+            }
+
+            override fun onError(exception: FacebookException) {
+                Timber.d("onError $exception")
+            }
+        })
+
+        val loginBehaviour = LoginBehavior.WEB_ONLY
+        loginManager.loginBehavior = loginBehaviour
+
+        loginManager.logOut()
+        val permissions = listOf(
+            "public_profile",
+            "email",
+            "pages_manage_posts")
+        loginManager.logIn(activity, permissions)
     }
 }
