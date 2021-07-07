4a86f4c49d277edcf84bcd6c8b50232f141bd1d4
YEAHIA MD. ARIF <yeahia.arif@gmail.com>
2021-07-07 14:04:28 +0600

    instagram media publish api implement init

--- Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/ui/ScheduleFragment.kt
+++ Project Code/SocialPostScheduler/app/src/main/java/study/cse499/socialpostscheduler/ui/ScheduleFragment.kt
@@ -1,5 +1,6 @@
 package study.cse499.socialpostscheduler.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.activity.OnBackPressedCallback
@@ -16,6 +17,7 @@ import kotlinx.serialization.json.Json
 import org.json.JSONObject
 import study.cse499.socialpostscheduler.R
 import study.cse499.socialpostscheduler.other.facebook_page.FacebookPageList
+import study.cse499.socialpostscheduler.other.instagram_page.InstagramPage
 import study.cse499.socialpostscheduler.viewmodel.ScheduleViewModel
 import java.util.*
 
@@ -23,6 +25,8 @@ import java.util.*
 class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
     lateinit var viewModel : ScheduleViewModel
     private val args: ScheduleFragmentArgs by navArgs()
+    var facebookLogin: Boolean = false;
+    var instagramLogin: Boolean = false;
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ScheduleViewModel::class.java)
@@ -32,7 +36,23 @@ class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
             }
         }
 
-        val response = args.accessToken
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
+        sharedPref?.let {
+            val response = args.accessToken
+            instagramLogin = it.getBoolean("isInstagram",false)
+            facebookLogin = it.getBoolean("isFacebook",false)
+
+            if(facebookLogin){
+                facebookPost(response)
+            }else{
+                instagramPost(response)
+            }
+
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    fun facebookPost(response: String){
         val obj = Json.decodeFromString<FacebookPageList>(response)
         val objectData = obj.data[0]
         val accessTokenUser = AccessToken.getCurrentAccessToken()
@@ -52,7 +72,6 @@ class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
                 null
             )
         }
-
         btSavePost.setOnClickListener {
             viewModel.insertSchedulePost(etPostContent.text.toString(), Calendar.getInstance().time)
             val request = GraphRequest.newPostRequest(
@@ -63,6 +82,20 @@ class ScheduleFragment : Fragment(R.layout.fragment_schedule) {
             }
             request.executeAsync()
         }
-        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    fun instagramPost(response: String){
+        val obj = Json.decodeFromString<InstagramPage>(response)
+        val accessTokenUser = AccessToken.getCurrentAccessToken()
+        btSavePost.setOnClickListener {
+            viewModel.insertSchedulePost(etPostContent.text.toString(), Calendar.getInstance().time)
+            val request = GraphRequest.newPostRequest(
+                accessTokenUser,
+                "/${obj.instagram_business_account.id}/feed",
+                JSONObject("{\"image_url\":\"https://fastly.4sqi.net/img/general/200x200/zNv7bbjkQQNgGArHKtAeb0O9BMJ1zdFrE-EpUx3-YU0.jpg\"}")){
+
+            }
+            request.executeAsync()
+        }
     }
 }
